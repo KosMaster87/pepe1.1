6574b41cde13b471107b1dfded788522ee707298
Konstantin Aksenov <kav.pbg@gmail.com>
2024-05-17 18:35:08 +0200

    check

--- js/game.js
+++ js/game.js
@@ -4,7 +4,6 @@ let canvas;
 let world;
 let keyboard;
 let character;
-// let keyboard = new Keyboard();
 
 /**
  * new World loud the canvas in world.class.ja
@@ -13,11 +12,8 @@ function init() {
   canvas = document.getElementById("canvas");
   keyboard = new Keyboard();
   world = new World(canvas, keyboard);
-  // character = new Character(world);
 
   // console.log(world.character);
   // console.log(world.enemies);
-
-  
 }
 
